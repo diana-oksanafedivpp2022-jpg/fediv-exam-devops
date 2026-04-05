0e81993e427cfa4ef91ee334638c6898d8746b28
unknown <diana-oksana.fediv.pp.2022@lpnu.ua>
2026-04-05 15:06:15 +0300

    feat: implement Terraform infrastructure-as-code and GitHub Actions CI/CD pipelines for DigitalOcean deployment

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-﻿terraform {
+terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
@@ -7,67 +7,95 @@
   }
   backend "s3" {
     endpoint                    = "fra1.digitaloceanspaces.com"
-    region                      = "us-east-1" 
-    bucket                      = "fediv-exam-state"
+    region                      = "us-east-1" # Required for DO Spaces
+    bucket                      = "fediv-terraform-state"
     key                         = "terraform.tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
   }
 }
 
-variable "do_token" {}
+variable "do_token" {
+  description = "DigitalOcean API Token"
+  sensitive   = true
+}
 
 provider "digitalocean" {
   token = var.do_token
 }
 
+# 1. Virtual Private Cloud (VPC)
 resource "digitalocean_vpc" "vpc" {
   name     = "fediv-vpc"
-  region   = "fra1"
+  region   = "fra1" # Closest to Ukraine
   ip_range = "10.10.10.0/24"
 }
 
+# 2. Firewall configuration
 resource "digitalocean_firewall" "fw" {
   name        = "fediv-firewall"
   droplet_ids = [digitalocean_droplet.node.id]
 
+  # Inbound connections
   inbound_rule {
     protocol         = "tcp"
-    port_range       = "22"
-    source_addresses = ["0.0.0.0/0"]
-  }
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0"]
-  }
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0"]
-  }
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "8000-8003"
+    port_range       = "22" # SSH
     source_addresses = ["0.0.0.0/0"]
   }
 
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80" # HTTP
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443" # HTTPS
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8000-8003" # Custom ports 8000; 8001; 8002; 8003
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  # Outbound connections (All ports)
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
-    destination_addresses = ["0.0.0.0/0"]
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
 
+# 3. Virtual Machine (Droplet)
 resource "digitalocean_droplet" "node" {
   name     = "fediv-node"
-  size     = "s-2vcpu-4gb"
+  size     = "s-2vcpu-4gb" # Meets Minikube/Kubernetes reqs
   image    = "ubuntu-24-04-x64"
   region   = "fra1"
   vpc_uuid = digitalocean_vpc.vpc.id
 }
 
-resource "digitalocean_spaces_bucket" "exam_bucket" {
+# 4. Storage Bucket
+resource "digitalocean_spaces_bucket" "bucket" {
   name   = "fediv-bucket"
   region = "fra1"
 }
+
+output "droplet_ip" {
+  value = digitalocean_droplet.node.ipv4_address
+}
+
