69204c625436609c82a296c899820f182d6b4cbe
unknown <diana-oksana.fediv.pp.2022@lpnu.ua>
2026-04-05 15:11:13 +0300

    fix bucket

--- main.tf
+++ main.tf
@@ -6,12 +6,16 @@ terraform {
     }
   }
   backend "s3" {
-    endpoint                    = "fra1.digitaloceanspaces.com"
-    region                      = "us-east-1" # Required for DO Spaces
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+    region                      = "us-east-1"
     bucket                      = "fediv-terraform-state"
     key                         = "terraform.tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
