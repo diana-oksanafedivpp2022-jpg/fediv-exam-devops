07cb096b1770af8e56e32b914ae89b8f071337fe
unknown <diana-oksana.fediv.pp.2022@lpnu.ua>
2026-04-05 15:16:38 +0300

    task1

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    endpoints = {
+    endpoints {
       s3 = "https://fra1.digitaloceanspaces.com"
     }
     region                      = "us-east-1"
@@ -16,6 +16,7 @@ terraform {
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
+    use_path_style              = true
   }
 }
 
