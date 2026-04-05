b52833bfa0a833a8e83a430f2b95c354232b025f
unknown <diana-oksana.fediv.pp.2022@lpnu.ua>
2026-04-05 16:46:22 +0300

    task 3

--- main.tf
+++ main.tf
@@ -74,25 +74,25 @@ resource "digitalocean_firewall" "fw" {
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22" # SSH
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80" # HTTP
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "443" # HTTPS
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "8000-8003" # Custom ports 8000; 8001; 8002; 8003
-    source_addresses = ["0.0.0.0/0"]
+    source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   # Outbound connections (All ports)
